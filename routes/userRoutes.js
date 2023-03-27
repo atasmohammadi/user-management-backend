73f377663879d2ac181f407a57d7c7535b4d61e6
Ata Mohammadi <atasmohammadi@gmail.com>
2023-03-27 12:18:49 +0200

    Setup modify user endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -18,4 +18,30 @@ router.get("/:id", checkTokenAdmin, (req, res) => {
   });
 });
 
+router.put("/", checkTokenAdmin, async ({ body, user }, res) => {
+  const { id, permissions } = body;
+
+  try {
+    if (!permissions) {
+      return res.sendStatus(400).send({ message: "all fields are required" });
+    }
+
+    const user = await User.findById(id).exec();
+
+    if (!user) {
+      return res.sendStatus(404).send({ message: "User not found" });
+    }
+
+    const { id: _, ...rest } = foundAdmin.toJSON();
+    await user.replaceOne({
+      ...rest,
+      permissions: permissions || rest.permissions,
+    });
+
+    return res.sendStatus(200).send({ message: "User updated" });
+  } catch (err) {
+    res.sendStatus(500).send({ message: err });
+  }
+});
+
 module.exports = router;
